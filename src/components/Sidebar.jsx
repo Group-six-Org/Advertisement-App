9a6bf50afc4e7ed44675fc85358920ba0830fd58
Alice Dawson <Alice Dawson>
2025-03-20 09:39:28 +0000

    advertisement project

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import K from "../constants";
+import { NavLink } from "react-router";
+
+const Sidebar = () => {
+  return (
+    <div className="flex flex-col w-60 gap-y-6 bg-amber-500 h-screen px-2 py-4 fixed left-0 top-0">
+      <span>Logo Here</span>
+      <div className="flex flex-col gap-y-6">
+        {K.NAVLINKS.map((link, index) => {
+          return (
+            <NavLink
+              key={index}
+              to={link.path}
+              className="text-white hover:bg-white hover:text-black px-3 py-2 w-full rounded-md flex gap-x-2 items-center"
+            >
+              <link.icon />
+              <span> {link.name}</span>
+            </NavLink>
+          );
+        })}
+      </div>
+      <button className="mt-auto">Log Out</button>
+    </div>
+  );
+};
+
+export default Sidebar;
